82647a1dd35ecaf8a4b82732e2f94500830ba63f
songusera <202300300005@mail.sdu.edu.cn>
2024-04-16 23:24:37 +0800

    课表界面加入

---
+++ src/main/java/org/example/javafx/controller/CourseController.java
@@ -0,0 +1,15 @@
+package org.example.javafx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+
+public class CourseController {
+    @FXML
+    private GridPane course;
+    @FXML
+    public void initialize() {
+
+    }
+
+}
