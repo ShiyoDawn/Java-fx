7849b3216c72470937cd1a61f0bea29bab4b1b22
SuperHZX666 <202300300017@mail.sdu.edu.cn>
2024-04-23 12:22:54 +0800

    初始化学生管理界面

--- src/main/java/org/example/javafx/controller/StudentTableController.java
+++ src/main/java/org/example/javafx/controller/StudentTableController.java
@@ -1,18 +1,25 @@
 package org.example.javafx.controller;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.MapValueFactory;
+import javafx.scene.image.ImageView;
+import org.example.javafx.pojo.Result;
+import org.example.javafx.request.DataRequest;
+import org.example.javafx.request.HttpRequestUtils;
+
+import java.util.List;
+import java.util.Map;
 
 public class StudentTableController {
 
     @FXML
     private Button add;
-
-    @FXML
-    private Button cancel;
 
     @FXML
     private TextField classText;
@@ -42,6 +49,9 @@ public class StudentTableController {
     private TableColumn id;
 
     @FXML
+    private ImageView image;
+
+    @FXML
     private TableColumn majorColumn;
 
     @FXML
@@ -66,11 +76,22 @@ public class StudentTableController {
     private TextField student_nameText;
 
     @FXML
-    private Button update;
+    private TableView tableView;
 
     @FXML
+    private Button update;
+    @FXML
     public void initialize(){
-
+        id.setCellValueFactory(new MapValueFactory<>("id"));
+        person_idColumn.setCellValueFactory(new MapValueFactory<>("person_id"));
+        student_nameColumn.setCellValueFactory(new MapValueFactory<>("student_name"));
+        departmentColumn.setCellValueFactory(new MapValueFactory<>("department"));
+        classesColumn.setCellValueFactory(new MapValueFactory<>("classes"));
+        gradeColumn.setCellValueFactory(new MapValueFactory<>("grade"));
+        majorColumn.setCellValueFactory(new MapValueFactory<>("major"));
+        DataRequest req = new DataRequest();
+        Result studentResult = HttpRequestUtils.request("/student/getStudentList", req);
+        List<Map> studentMap = (List<Map>) studentResult.getData();
+        tableView.setItems(FXCollections.observableList(studentMap));
     }
-
 }
